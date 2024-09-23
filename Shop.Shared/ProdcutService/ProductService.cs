50e83371423de4e59b3224670ce781d348601bc4
Tomasz Leś <tomasz.les@pw.edu.pl>
2024-09-23 12:51:22 +0200

    q

--- Shop.Shared/ProdcutService/ProductService.cs
+++ Shop.Shared/ProdcutService/ProductService.cs
@@ -1,9 +1,14 @@
-﻿using Shop.Shared.Models;
+﻿using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Shop.Shared.Configuration;
+using Shop.Shared.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Shop.Shared.ProdcutService
@@ -11,11 +16,12 @@ namespace Shop.Shared.ProdcutService
     public class ProductService : IProductService
     {
         private readonly HttpClient _httpClient;
-        private readonly
+        private readonly AppSettngs _appSettngs;
 
-        public ProductService(HttpClient httpClient)
+        public ProductService(HttpClient httpClient, IOptions<AppSettngs> appSettings)
         {
             _httpClient = httpClient;
+            _appSettngs = appSettings.Value;
         }
 
         public Task<ServiceReponse<Product>> CreateProductServiceReponse(Product product)
@@ -37,7 +43,19 @@ namespace Shop.Shared.ProdcutService
         {
             try
             {
-                var reponse = await _httpClient.GetAsync("api/product");
+                var reponse = await _httpClient.GetAsync(_appSettngs.ProductEndpoint.GetProducts);
+                reponse.EnsureSuccessStatusCode();
+                var json = await reponse.Content.ReadAsStringAsync();
+                var products = JsonConvert.DeserializeObject<ServiceReponse<List<Product>>>(json);
+                return products;
+            }
+            catch (HttpRequestException ex)
+            {
+                return new ServiceReponse<List<Product>>()
+                {
+                    Success = false,
+                    Message = "Failed to load products:" + ex.Message
+                };
             }
             catch (Exception)
             {
